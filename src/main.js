09b4e32ba2e200dbf1374a0b5a6dae7cb6127da0
pilarguz10 <pilar1494@hotmail.com>
2021-02-14 21:31:26 +0000

    Proyecto para enviar

--- src/main.js
+++ src/main.js
@@ -4,7 +4,39 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+// Array con los posibles "palos" de la carta
+const suits = ["\u2666", "\u2665", "\u2660", "\u2663"];
+// Array con los posibles "números" de la carta
+const number = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+// Variable donde guardo el div del palo de arriba
+const divSuitA = document.querySelector("#suitA");
+// Variable donde guardo el div del palo de abajo
+const divSuitB = document.querySelector("#suitB");
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  // Con esta función genero un item al azar del array number
+  let randomIndex = array => {
+    let radomNumber = Math.floor(Math.random() * array.length);
+    return array[radomNumber];
+  };
+
+  let cardColor = suit => {
+    if (suit == "\u2666" || suit == "\u2665") {
+      let redColorTop = divSuitA;
+      let redColorBottom = divSuitB;
+      redColorTop.className = "suitTop text-danger";
+      redColorBottom.className = "suitBottom text-danger";
+    } else {
+      let blackColorTop = divSuitA;
+      let blackColorBottom = divSuitB;
+      blackColorTop.className = "suitTop text-dark";
+      blackColorBottom.className = "suitBottom text-dark";
+    }
+  };
+  let suitRandomIndex = randomIndex(suits);
+  cardColor(suitRandomIndex);
+  let divNumber = document.querySelector("#cardNumber");
+  divSuitA.innerHTML = suitRandomIndex;
+  divSuitB.innerHTML = suitRandomIndex;
+  divNumber.innerHTML = randomIndex(number);
 };
